31d68e65145fa4404d08850e62613642a82da2b2
Toby Zethoven <tzethoven@gmail.com>
2019-04-01 19:50:54 +0100

    deploy

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-
+const path = require('path');
 const items = require('./routes/api/items');
 
 
@@ -13,6 +13,7 @@ app.use(bodyParser.json());
 // DB Config
 const db = require('./config/keys').mongoURI;
 
+// Connect to Mongo
 mongoose
     .connect(db, options={ useNewUrlParser: true })
     .then(() => console.log('MongoDB Connected...'))
@@ -20,6 +21,15 @@ mongoose
 
 // Use Routes
 app.use('/api/items', items)
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const port = process.env.PORT || 5000;
 
